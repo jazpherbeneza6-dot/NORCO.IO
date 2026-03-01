ced9b77ba31b1dd4b2533cbbfdad36cee1a6bfa4
Jazz/renz <jazpherbeneza6@gmail.com>
2026-03-01 19:29:52 +0800

    Make clothing images fill modal properly

--- script.js
+++ script.js
@@ -243,13 +243,11 @@ function toggleModalImage(productId) {
   setTimeout(() => {
     if (showingClothingOnly) {
       img.src = product.clothingImage;
-      img.style.objectFit = 'contain';
       imageContainer.classList.add('clothing-view');
       btn.innerHTML = '<span class="material-icons-outlined">person</span>';
       btn.title = 'View with model';
     } else {
       img.src = product.image;
-      img.style.objectFit = 'cover';
       imageContainer.classList.remove('clothing-view');
       btn.innerHTML = '<span class="material-icons-outlined">checkroom</span>';
       btn.title = 'View clothing only';
